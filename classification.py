db741d70f3a994495d128aaba439e650f5f4ce0f
RISHAB <rishab2911@gmail.com>
2018-04-06 14:17:55 +0530

    Accuracy
    Done

---
+++ classification.py
@@ -0,0 +1,43 @@
+import numpy as np
+import csv
+from sklearn.naive_bayes import GaussianNB
+from sklearn import tree
+from sklearn import ensemble
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
+
+lines = csv.reader(open("features.csv", "r"))
+#testdata = csv.reader(open("R:\data.csv", "r"))
+dataset = list(lines)
+#test_data = np.array(list(testdata)).astype(np.float)
+data = np.array(dataset)
+x = data[1:, 1:8]
+x = x.astype(np.float)
+y = data[1:,8]
+y = y.astype(np.float)
+
+max = 0
+
+for i in range(100):
+	X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=45)
+	#clf = SVC()
+	#clf = GaussianNB()
+	clf = ensemble.RandomForestClassifier()
+	#clf = tree.DecisionTreeClassifier()
+	clf.fit(X_train, y_train) 
+	y_pred = clf.predict(X_test)
+	#print(X_test)
+	#print(y_pred)
+	acc = accuracy_score(y_test, y_pred)
+	if(max<acc):
+		max = acc
+		joblib.dump(clf, 'SVMClassifier.pkl') 
+	print("SVMClassifier")
+	print(max)
+
+
+
+
+
